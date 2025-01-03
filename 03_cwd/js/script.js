53540ac0024e81881a9637cf2d28f7859a311cb8
wi <whyeil29@gmail.com>
2025-01-03 16:45:45 +0900

    0103

--- 03_cwd/js/script.js
+++ 03_cwd/js/script.js
@@ -1,3 +1,4 @@
+// 모달 동작
 const firstListItem = document.querySelector("#tab1 li:first-child");
 const modalBox = document.querySelector("#modal");
 const modalBtn = modalBox.querySelector("button");
@@ -12,4 +13,27 @@ function modalOpened(){
 function modalClosed(){
   modalBox.style.display = "none";
   console.log("모달창이 닫혔어요!");
+}
+
+
+// 비디오 버튼 동작
+const vidBox = document.querySelector(".vid");
+const vid = vidBox.querySelector("video");
+const vidBtn = vidBox.querySelector("button i");
+let toggleSwitch = 1;
+
+vidBtn.onclick = vidToggle;
+
+function vidToggle() {
+  if (toggleSwitch === 1) {
+    vidBtn.setAttribute("class", "fa-solid fa-play");
+    vid.pause();
+    toggleSwitch = 0;
+    console.log("비디오가 일시정지 됩니다.");
+  } else {
+    vidBtn.setAttribute("class", "fa-solid fa-pause");
+    vid.play();
+    toggleSwitch = 1;
+    console.log("비디오가 재생 됩니다.");
+  }
 }
